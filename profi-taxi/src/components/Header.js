5538f54bd904d04705de84321129b1ea79bc0d79
Jakub Dvořák <jakubdvorak91@seznam.cz>
2023-03-30 09:37:02 +0200

    header finished profi taxi

--- profi-taxi/src/components/Header.js
+++ profi-taxi/src/components/Header.js
@@ -2,10 +2,12 @@ import "./header.css"
 import Enyaq from "./assets/enyaq.png"
 import BackgroundVideo from "./assets/background-vid.mp4"
 import Logo from "./assets/logo-nobg.png"
+import React from "react"
+import { SiFacebook } from "react-icons/si"
+import { GrInstagram } from "react-icons/gr"
+import { ImYoutube } from "react-icons/im"
 
 const Header = () => {
-    // document.querySelector("#background-video").play()
-
     return (
         <div className="header__container">
             <video
@@ -16,11 +18,29 @@ const Header = () => {
                 muted
                 loop
             ></video>
+            <a href="#">
+                <img src={Logo} alt="profi taxi logo" id="profi-taxi-logo" />
+            </a>
 
-            <img src={Logo} alt="profi taxi logo" id="profi-taxi-logo" />
+            <div className="header__social-icons">
+                <a href="#">
+                    <SiFacebook />
+                </a>
+                <a href="#">
+                    <GrInstagram />
+                </a>
+                <a href="#">
+                    <ImYoutube />
+                </a>
+            </div>
             <div className="header__text-part">
-                <h1>Profi Taxi</h1>
+                <h1>Profi Taxi Mladá Boleslav</h1>
                 <h3>+420 777 777 777</h3>
+                <p>První TAXI s elektrovozem ve flotile vozů taxislužby.</p>
+                <p>Luxusní vozy taxi za standardní ceny</p>
+                <p>
+                    <strong>Chtějte za své peníze více...</strong>
+                </p>
                 <div className="header__text-part__buttons">
                     <a className="btn" href="#">
                         Zavolejte
