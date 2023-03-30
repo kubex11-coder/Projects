649f7782238b4c8ff518da98d00e669a180a4aa3
Jakub Dvořák <jakubdvorak91@seznam.cz>
2023-03-30 17:51:09 +0200

    nová komponenta contacts profi taxi

--- profi-taxi/src/App.js
+++ profi-taxi/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/header/Header"
 import Prices from "./components/ceník/Prices"
+import Contacts from "./components/contacts/Contacts"
 
 const App = () => {
     return (
         <div className="container">
             <Header />
             <Prices />
+            <Contacts />
         </div>
     )
 }
---
+++ profi-taxi/src/components/contacts/Contacts.js
@@ -0,0 +1,7 @@
+import "./contacts.css"
+
+const Contacts = () => {
+    return <h1>test</h1>
+}
+
+export default Contacts
